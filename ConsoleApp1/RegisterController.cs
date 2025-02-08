a7512ba8bf5011927bdeea998370826b4a113571
Aleksandr Komaral <komaraltok@gmail.com>
2025-02-08 19:04:40 +0900

    В клиентском приложений переделан фунционал регистрации на http.

--- ConsoleApp1/RegisterController.cs
+++ ConsoleApp1/RegisterController.cs
@@ -4,8 +4,10 @@ using System.Diagnostics;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using ConsoleApp1.Models;
+using ConsoleApp1.Models.Dto;
 
 namespace ConsoleApp1;
 
@@ -24,26 +26,52 @@ internal class RegisterController
             Console.WriteLine("Такой логин уже существует!");
             return;
         }
-        
+
         Console.WriteLine("Придумайте пароль:");
         string password = Console.ReadLine();
 
         Console.WriteLine("Как вас зовут?");
         string name = Console.ReadLine();
 
-        var user = new User();
-        user.Name = name;
-        user.Password = password;
-        user.Login = userLogin;
-        user.Id = DataBase.Users.Count + 1;
+        UserRegister(userLogin, password, name);
 
-        DataBase.Users.Add(user);
-        DataBase.Save();
-        
         Console.WriteLine("Спасибо за регистрацию!");
-
     }
-    
+
+    public User UserRegister( string userLogin, string password, string name)
+    {
+        var client = new HttpClient();
+        var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:5227/login/register");
+        var requestModel = new
+        {
+            Login = userLogin,
+            Password = password,
+            Name = name,
+        };
+        string json = JsonSerializer.Serialize(requestModel);
+        var content = new StringContent(json, null, "application/json");
+        request.Content = content;
+        var response = client.Send(request);
+
+        string responseJson = response.Content.ReadAsStringAsync().Result;
+        var result = JsonSerializer.Deserialize<LoginResponce>(responseJson);
+        if (result.Success == true)
+        {
+            return new User
+            {
+                Id = result.Result.Id,
+                Name = result.Result.Name,
+                Password = password,
+                Login = userLogin,
+            };
+        }
+        else
+        {
+            Console.WriteLine(result.Description);
+            return null;
+        }
+    }
+
     private bool CheckLoginFree(string login)
     {
         foreach (var user in DataBase.Users)
